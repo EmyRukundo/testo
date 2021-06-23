d3eec54604c13f266c0a1386b0ab53a491f87198
Emmanuel RUKUNDO <e.rukundo@bk.rw>
2021-06-23 21:55:52 +0200

    articles list items

---
+++ pages/article/index.js
@@ -0,0 +1,8 @@
+const article = () => {
+    return (
+        <div>
+                  This is an articl
+        </div>
+    )
+}
+export default article;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,28 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import ArticleList from "../components/ArticleList";
+
 
 
 
 // you can't impornt main css in component, you must create a css component and with .module.css
-export default function Home() {
+export default function Home({articles}) {
   return (
     <div>
-        <head>
+        <Head>
           <title>EmyTheProgrammer</title>
           <meta name='keywords' content='web development, programming'/>
-        </head>
-        <h1>web development</h1>
+        </Head>
+        <ArticleList article={articles} />
     </div>
   )
 }
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+  const articles = await res.json()
+  return {
+    props: {
+      articles
+    }
+  }
+}
+
