2b919db543f7d6b7f0c8ca00089e810ff4b8cd0d
EmyRukundo <39124680+EmyRukundo@users.noreply.github.com>
2021-07-12 22:06:29 +0200

    Revert "Branch_Custom Meta Component "

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head';
-import ArticleList from "../components/ArticleList";
-
+import Head from 'next/head'
+import ArticleList from '../components/ArticleList'
+import server from '../config/index'
 
 
 
@@ -8,21 +8,29 @@ import ArticleList from "../components/ArticleList";
 export default function Home({articles}) {
   return (
     <div>
-        <Head>
-          <title>EmyTheProgrammer</title>
-          <meta name='keywords' content='web development, programming'/>
-        </Head>
-        <ArticleList article={articles} />
+
+        <ArticleList articles={articles} />
     </div>
   )
 }
-export const getStaticProps = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+export const getStaticProps = async() => {
+  const res = await fetch(`http://localhost:3000/api/articles`)
   const articles = await res.json()
+
   return {
     props: {
-      articles
+      articles,
     }
   }
 }
 
+// export const getStaticProps = async() => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+//   const articles = await res.json()
+
+//   return {
+//     props: {
+//       articles,
+//     }
+//   }
+// }
